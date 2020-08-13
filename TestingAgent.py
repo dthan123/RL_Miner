c1a2df202dc8088ce7233d2b78cf32cf26819c68
dthan123 <55889151+dthan123@users.noreply.github.com>
2020-08-14 00:31:18 +0100

    Add files via upload

---
+++ TestingAgent.py
@@ -0,0 +1,65 @@
+from warnings import simplefilter 
+simplefilter(action='ignore', category=FutureWarning)
+
+import sys
+from keras.models import model_from_json
+from MinerEnv import MinerEnv
+import numpy as np
+
+ACTION_GO_LEFT = 0
+ACTION_GO_RIGHT = 1
+ACTION_GO_UP = 2
+ACTION_GO_DOWN = 3
+ACTION_FREE = 4
+ACTION_CRAFT = 5
+
+HOST = "localhost"
+PORT = 1111
+if len(sys.argv) == 3:
+    HOST = str(sys.argv[1])
+    PORT = int(sys.argv[2])
+
+# load json and create model
+json_file = open('/Users/duythan/Python/FPT_RL/rlcomp2020/Miner-Testing-CodeSample/build/DQNmodel_20200810-0103_ep1000.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+DQNAgent = model_from_json(loaded_model_json)
+# load weights into new model
+DQNAgent.load_weights("/Users/duythan/Python/FPT_RL/rlcomp2020/Miner-Training-Local-CodeSample/TrainedModels/DQNmodel_20200813-2235_ep10000.h5")
+print("Loaded model from disk")
+status_map = {0: "STATUS_PLAYING", 1: "STATUS_ELIMINATED_WENT_OUT_MAP", 2: "STATUS_ELIMINATED_OUT_OF_ENERGY",
+                  3: "STATUS_ELIMINATED_INVALID_ACTION", 4: "STATUS_STOP_EMPTY_GOLD", 5: "STATUS_STOP_END_STEP"}
+try:
+    # Initialize environment
+    minerEnv = MinerEnv(HOST, PORT)
+    minerEnv.start()  # Connect to the game
+    MAP_MAX_X = 21 #Width of the Map
+    MAP_MAX_Y = 9  #Height of the Map
+    mapID = np.random.randint(1, 6) #Choosing a map ID from 5 maps in Maps folder randomly
+    posID_x = np.random.randint(MAP_MAX_X) #Choosing a initial position of the DQN agent on X-axes randomly
+    posID_y = np.random.randint(MAP_MAX_Y) #Choosing a initial position of the DQN agent on Y-axes randomly
+    #Creating a request for initializing a map, initial position, the initial energy, and the maximum number of steps of the DQN agent
+    request = ("map" + str(mapID) + "," + str(posID_x) + "," + str(posID_y) + ",50,100") 
+    #Send the request to the game environment (GAME_SOCKET_DUMMY.py)
+    minerEnv.send_map_info(request)
+
+    minerEnv.reset()
+    s = minerEnv.get_state()  ##Getting an initial state
+    while not minerEnv.check_terminate():
+        try:
+            action = np.argmax(DQNAgent.predict(s.reshape(1, len(s))))  # Getting an action from the trained model
+            print("next action = ", action)
+            minerEnv.step(str(action))  # Performing the action in order to obtain the new state
+            print(minerEnv.get_reward())
+            s_next = minerEnv.get_state()  # Getting a new state
+            s = s_next
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            print("Finished.")
+            break
+    print(status_map[minerEnv.state.status])
+except Exception as e:
+    import traceback
+    traceback.print_exc()
+print("End game.")
